fc8d935fc5a06d9e4dbd57b961712d9c77cd520a
ghxm <max.haag@gmx.net>
2022-09-15 13:01:00 +0200

    add restream script

---
+++ create_restream_schedule_json.py
@@ -0,0 +1,63 @@
+import argparse
+import json
+import icalendar
+import requests
+import re
+import sys
+import pytz
+
+
+
+
+def main(debug = False):
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-icur','--input-current', required=not debug, default= None, help='Input .json file (can be url)')
+    parser.add_argument('-inext','--input-next', required=False, default=None, help='Input .json file (can be url)')
+    parser.add_argument('-o', '--output', default='./restream.json', required=not debug, help='Output .json file')
+
+    args = parser.parse_args()
+
+    ## debug
+    if sys.gettrace() is not None:
+        args.input_current = 'http://116.203.52.102/cfm-fallback/current.json'
+        args.input_next = 'http://116.203.52.102/cfm-fallback/next.json'
+
+
+    restream = {'current': None,
+                'next': None}
+
+    if args.input_current is not None:
+        if args.input_current.startswith('http'):
+            current = requests.get(args.input_current).json()
+        else:
+            current = json.load(open(args.input_current, 'rb'))
+
+        restream['current'] = dict()
+
+        restream['current']['show_title'] = current['song']
+        restream['current']['description'] = None
+        restream['current']['on_air_timestamp'] = current['on_air_timestamp']
+
+
+    if args.input_next is not None:
+        if args.input_next.startswith('http'):
+            next = requests.get(args.input_next).json()
+        else:
+            next = json.load(open(args.input_next, 'rb'))
+
+        restream['next'] = dict()
+
+        restream['next']['show_title'] = next['song']
+        restream['next']['description'] = None
+
+
+    # write out json file
+    with open(args.output, 'w') as f:
+        json.dump(restream, f, indent=4)
+
+if __name__ == '__main__':
+    if sys.gettrace() is not None:
+        main(debug=True)
+    else:
+        main()
